469b9573f1dc611befeedd75df2d5133157fba32
emmanuelneri <%monst3rk1ll%>
2019-05-18 17:52:01 -0300

    alterando parametro para ConsumerRecord no listener

--- consumer/src/main/java/br/com/emmanuelneri/consumer/OrderConsumer.java
+++ consumer/src/main/java/br/com/emmanuelneri/consumer/OrderConsumer.java
@@ -2,8 +2,8 @@ package br.com.emmanuelneri.consumer;
 
 import br.com.emmanuelneri.consumer.model.Order;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,7 +11,10 @@ import org.springframework.stereotype.Component;
 public class OrderConsumer {
 
     @KafkaListener(topics = "${order.topic}", groupId = "${spring.kafka.consumer.group-id}")
-    public void consumer(@Payload Order order) {
-        log.info("Order: " + order);
+    public void consumer(final ConsumerRecord<String, Order> consumerRecord) {
+        log.info("key: " + consumerRecord.key());
+        log.info("Headers: " + consumerRecord.headers());
+        log.info("Partion: " + consumerRecord.partition());
+        log.info("Order: " + consumerRecord.value());
     }
 }
