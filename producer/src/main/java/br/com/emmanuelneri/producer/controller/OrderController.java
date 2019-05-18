8370da4383b793510a1d5176d4065f3a9ef56460
emmanuelneri <%monst3rk1ll%>
2019-05-18 17:11:19 -0300

    melhorando nome do consumer group

--- producer/src/main/java/br/com/emmanuelneri/producer/controller/OrderController.java
+++ producer/src/main/java/br/com/emmanuelneri/producer/controller/OrderController.java
@@ -2,9 +2,6 @@ package br.com.emmanuelneri.producer.controller;
 
 import br.com.emmanuelneri.producer.OrderProducer;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
